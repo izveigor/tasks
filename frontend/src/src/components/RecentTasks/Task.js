cc6a3ce492d24ed3b1e6128e87a3409040f5f713
izveigor <izveigor@mail.ru>
2022-11-25 20:16:34 +0300

    Добавлен тест components/RecentTasks/Task.test.js

--- frontend/src/src/components/RecentTasks/Task.js
+++ frontend/src/src/components/RecentTasks/Task.js
@@ -8,11 +8,11 @@ export default function Task(props) {
             <hr className="border-gray-300" />
             <div className="mt-5 w-50 px-2 py-2 flex justify-center">
                 <div className="ml-3 w-16 h-16">
-                    <img data-testid="task-image-test" className="rounded-full align-middle" src={USERS_URL_WITHOUT_SLASH + props.image} />
+                    <img data-testid="task-image" className="rounded-full align-middle" src={USERS_URL_WITHOUT_SLASH + props.image} />
                 </div>
                 <div className="grid md:grid-rows-2 grid-rows-3 ml-3">
-                    <span data-testid="task-name-test" className="text-md md:row-span-1 row-span-2">{props.title} #{props.id}</span>
-                    <span data-testid="task-time-test" className="text-sm text-gray-500">{props.time}</span>
+                    <span data-testid="task-name" className="text-md md:row-span-1 row-span-2">{props.title} #{props.id}</span>
+                    <span data-testid="task-time" className="text-sm text-gray-500">{props.time}</span>
                 </div>
                 {(props.status == "Успешно") && (<div className="ml-8 h-100">
                     <div className="align-middle inline-block">
@@ -21,7 +21,7 @@ export default function Task(props) {
                                 <div className="animate-pulse inline-block align-middle rounded-full h-2 w-2 bg-green-600"></div>
                             </div>
                             <div className="h-100">
-                                <p data-testid="task-status-test" className="ml-2 align-middle inline-block text-sm text-green-600 w-[5em]">Успешно</p>
+                                <p data-testid="task-status" className="ml-2 align-middle inline-block text-sm text-green-600 w-[5em]">Успешно</p>
                             </div>
                         </div>
                     </div>
@@ -33,7 +33,7 @@ export default function Task(props) {
                                 <div className="animate-pulse inline-block align-middle rounded-full h-2 w-2 bg-orange-600"></div>
                             </div>
                             <div className="h-100">
-                                <p data-testid="task-status-test" className="ml-2 align-middle inline-block text-sm text-orange-600 w-[5em]">Обработка</p>
+                                <p data-testid="task-status" className="ml-2 align-middle inline-block text-sm text-orange-600 w-[5em]">Обработка</p>
                             </div>
                         </div>
                     </div>
@@ -45,7 +45,7 @@ export default function Task(props) {
                                 <div className="animate-pulse inline-block align-middle rounded-full h-2 w-2 bg-red-600"></div>
                             </div>
                             <div className="h-100">
-                                <p data-testid="task-status-test" className="ml-2 align-middle inline-block text-sm text-red-600 w-[5em]">Закрыто</p>
+                                <p data-testid="task-status" className="ml-2 align-middle inline-block text-sm text-red-600 w-[5em]">Закрыто</p>
                             </div>
                         </div>
                     </div>
---
+++ frontend/src/src/components/RecentTasks/Task.test.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import Task from './Task';
+import { USERS_URL_WITHOUT_SLASH } from '../../features/constants';
+
+
+it("Проверяем props", () => {
+    const propsData = 
+    [
+        {
+            'title': 'Сделать отчет о страусах',
+            'id': 1,
+            'time': '20 минут',
+            'image': 'https://image',
+            'status': 'Успешно',
+        }, 
+        {
+            'title': 'Сделать отчет о воробьях',
+            'id': 2,
+            'time': '30 минут',
+            'image': 'https://image2',
+            'status': 'Обработка',
+        }, 
+        {
+            'title': 'Сделать отчет о сороках',
+            'id': 3,
+            'time': '40 минут',
+            'image': 'https://image3',
+            'status': 'Закрыто',
+        },
+    ];
+
+    propsData.forEach((data) => {
+        console.log(data);
+        act(() => {
+            render(<Task title={data.title}
+                id={data.id}
+                time={data.time}
+                image={data.image}
+                status={data.status}
+            />);
+        });
+
+        console.log(data);
+        expect(
+            document.querySelector("[data-testid='task-name']").textContent
+        ).toEqual(data.title + " #" + data.id.toString());
+        expect(
+            document.querySelector("[data-testid='task-time']").textContent
+        ).toEqual(data.time);
+        expect(
+            document.querySelector("[data-testid='task-image']")
+        ).toHaveAttribute('src', USERS_URL_WITHOUT_SLASH + data.image);
+        expect(
+            document.querySelector("[data-testid='task-status']").textContent
+        ).toEqual(data.status);
+
+        document.body.innerHTML = "";
+    });
+});
