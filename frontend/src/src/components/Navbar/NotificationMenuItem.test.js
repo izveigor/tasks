1aeac06a52415e4d456b30bd5f340455ccf8937d
izveigor <izveigor@mail.ru>
2022-11-25 17:12:29 +0300

    Добавлен тест components/Navbar/NotificationMenuItem.test.js

---
+++ frontend/src/src/components/Navbar/NotificationMenuItem.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import NotificationMenuItem from './NotificationMenuItem';
+import { USERS_URL_WITHOUT_SLASH } from '../../features/constants';
+
+
+it("Проверяем props", () => {
+    const text = "Текст";
+    const image = "/image";
+    const time = "18:00";
+
+    act(() => {
+        render(<NotificationMenuItem text={text} image={image} time={time} />)
+    });
+
+    expect(
+        document.querySelector('[data-testid="notification-image"]')
+    ).toHaveAttribute('src', USERS_URL_WITHOUT_SLASH + image);
+
+    expect(
+        document.querySelector('[data-testid="notification-text"]').textContent
+    ).toEqual(text + " ");
+
+    expect(
+        document.querySelector('[data-testid="notification-time"]').textContent
+    ).toEqual(time);
+})
