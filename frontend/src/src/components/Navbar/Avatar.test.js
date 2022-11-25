79d17edf51416d89969c33dc15f8663648771917
izveigor <izveigor@mail.ru>
2022-11-25 11:03:02 +0300

    Добавлен тест components/Navbar/Avatar.test.js

---
+++ frontend/src/src/components/Navbar/Avatar.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import Avatar from './Avatar';
+import {
+    BrowserRouter as Router,
+} from 'react-router-dom';
+
+it("Меняем видимость меню профиля при нажатии", () => {
+    const onChange = jest.fn();
+    const avatar = {
+        "image": "http://image",
+    }
+
+    act(() => {
+        render(<Router><Avatar onChange={onChange} avatar={avatar} /></Router>);
+    });
+
+    const avatarButton = screen.getByRole('button');
+
+    expect(
+        document.querySelector('[data-testid="profile-menu-test"]') === null
+    ).toBe(true);
+
+    act(() => {
+        fireEvent.click(avatarButton);
+    });
+
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(
+        document.querySelector('[data-testid="profile-menu-test"]') !== null
+    ).toBe(true);
+});
