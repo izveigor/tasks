50040f2a4c07e4be402e11710c502784a957842a
izveigor <izveigor@mail.ru>
2022-12-29 15:58:18 +0300

    Исправлены тесты для микросервиса "account"

--- backend/account/src/teams/urls.py
+++ backend/account/src/teams/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from users import views
+from . import views
 
 urlpatterns = [
     path("authorization_like_teammate/", views.AuthorizationLikeTeammate.as_view()),  # type: ignore
